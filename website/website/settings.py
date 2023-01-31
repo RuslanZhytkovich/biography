09b66aa0534ec730c786c2c1af78cd60b2dec8c3
ruslan <rzhitkovich@bk.ru>
2023-01-31 19:38:47 +0300

    ПОдключил статические файлы

--- website/website/settings.py
+++ website/website/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
